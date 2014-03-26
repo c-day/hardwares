1e9bc9fdd6be7806c2e7961072f099f19acdca42
emassey2 <emassey2.wisc.edu>
2014-03-26 00:10:06 -0500

    added branch cases to .asm test

--- Project/noMemTests.asm
+++ Project/noMemTests.asm
@@ -6,7 +6,7 @@
 # All tests are perfomed assuming that LLB, SUB, and B EQ work properly
 #################################################################################
 
-#   We assume that BEQ and halt works
+#   We assume that BEQ, LLB, and halt works
 
 T_ADD:                  # test the ADD and SUB operations
     LLB R5, 0x05        # R5 <= 0x0005
@@ -82,10 +82,53 @@ T_LHB:
     SLL R2, R2, 8
     LHB R1, 0xBC
     SUB R0, R1, R2
-    B EQ, DONE          # if R1 == R2, test passed
+    B EQ, T_B_UNCOND    # if R1 == R2, test passed
     HLT                 # test failed
 
+    #well we know, or at least hope, that B EQ has worked so lets try the others
+T_B_UNCOND:
+    B UNCOND,  T_NEQ
 
+T_NEQ:
+    LLB R5, 0x09
+    LLB R1, 0x08
+    SUB R0, R5, R1
+    B NEQ, T_GT
+    HLT
+
+T_GT:   #9-8 is > 0 so we can just use the flags set above
+    B GT, T_GTE
+    HLT
+
+T_GTE:  #Test the greater part of GTE
+    B GTE, T_GTE2
+    HLT
+
+T_GTE2: #Test equal part of GTE
+    SUB R0, R9, R9
+    B GTE, T_LT
+    HLT
+
+T_LTE:  #Test equal part of LTE
+    B LTE, T_LTE2
+    HLT
+
+T_LTE2: #Test less than part of LTE
+    SUB R0, R1, R5  #8-9 < 0
+    B LT, T_LT
+    HLT
+
+T_LT:   #reuse flags set above
+    B LT, T_OVFL
+    HLT
+
+T_OVFL:
+    LLB R5, 0xFF    #sign extended on load to 0xFFFF
+    LLB R1, 0xFF
+    ADD R0, R1, R5
+    B OVFL, T_ADD2
+    HLT
+    
 # end of basic test
 
 *******************************************************************************
